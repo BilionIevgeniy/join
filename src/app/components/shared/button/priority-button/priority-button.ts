29354c609b845c9930381c774e228c8705a4d6e5
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-06-23 18:09:48 +0200

    feat:priority btn added

---
+++ src/app/components/shared/button/priority-button/priority-button.ts
@@ -0,0 +1,30 @@
+import { Component, ViewEncapsulation, input, output } from '@angular/core';
+import { TaskPriority } from '../../../../core/models/task.model';
+
+const PRIORITY_CONFIG: Record<TaskPriority, { label: string; color: string }> = {
+  urgent: { label: 'Urgent', color: '#FF3D00' },
+  medium: { label: 'Medium', color: '#FFA800' },
+  low: { label: 'Low', color: '#7AE229' },
+};
+
+@Component({
+  selector: 'app-priority-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './priority-button.html',
+  styleUrl: './priority-button.scss',
+  encapsulation: ViewEncapsulation.None,
+})
+export class PriorityButton {
+  priority = input.required<TaskPriority>();
+  selected = input<boolean>(false);
+  priorityChange = output<TaskPriority>();
+
+  get config() {
+    return PRIORITY_CONFIG[this.priority()];
+  }
+
+  onClick() {
+    this.priorityChange.emit(this.priority());
+  }
+}
